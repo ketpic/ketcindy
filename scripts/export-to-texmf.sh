2f0fb43800d595448ba0c284df3e522fc9f3d323
Munehiro Yamamoto <munepixyz@gmail.com>
2018-01-14 00:20:33 +0900

    export-to-texmf.sh, ketcindy.sh: imported from old-texlive

---
+++ scripts/export-to-texmf.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# export the ketcindy git to a texmf tree
+
+set -e
+
+if [ -d texmf ] ; then
+  echo "directory texmf already present, not exporting to it!\n" >&2
+  exit 1
+fi
+
+mkdir texmf
+git archive --format=tar --prefix=ketcindy/ HEAD | (cd texmf && tar xf -)
+mkdir -p texmf/doc/support/ketcindy
+mkdir -p texmf/scripts/ketcindy/ketfiles
+mkdir -p texmf/tex/latex/ketcindy
+
+cd texmf
+
+# tex hierarchy
+mv ketcindy/ketpicstyle/*.sty tex/latex/ketcindy
+rmdir ketcindy/ketpicstyle
+
+# scripts
+mv ketcindy/ketlib ketcindy/ketlibC ketcindy/ketoutset.txt ketcindy/setketcindy.txt \
+   ketcindy/data ketcindy/scripts/ketcindy.sh ketcindy/ketjava \
+   scripts/ketcindy
+mv ketcindy/ketfiles/allbuttons.cdy ketcindy/ketfiles/template.cdy scripts/ketcindy/ketfiles
+chmod 0755 scripts/ketcindy/ketcindy.sh
+
+# doc
+mv ketcindy/CindyJS ketcindy/ketmanual ketcindy/ketfiles/ketsample \
+   ketcindy/LICENSE ketcindy/source \
+   ketcindy/ketfiles/ScriptDraw.txt ketcindy/ketfiles/ScriptInitialization.txt \
+   ketcindy/ketfiles/ScriptKeytyped.txt \
+   doc/support/ketcindy
+for i in TODO README.TeXLive ; do
+  if [ -r ketcindy/$i ] ; then
+    mv ketcindy/$i doc/support/ketcindy
+  fi
+done
+
+# the rest should be files that can be removed:
+rm ketcindy/scripts/update-permissions.sh
+rm ketcindy/scripts/export-to-texmf.sh
+# now the scripts dir should be empty
+rmdir ketcindy/scripts
+
+# now the ketfiles dir should be empty
+rmdir ketcindy/ketfiles
+
+rm ketcindy/setketcindyr.txt
+rm ketcindy/setketcindysci.txt
+
+# now all should be gone
+rmdir ketcindy
+
+

5272f32c956791e99537f38b4c6ffab86acbf2b9
s-takato <takato@phar.toho-u.ac.jp>
2018-12-01 08:41:53 +0900

    v3.2.3(181201)

--- ketcindyfolder/scripts/ketlib/ketcindylibbasic1r.cs
+++ ketcindyfolder/scripts/ketlib/ketcindylibbasic1r.cs
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 
-println("KeTCindy V.3.2.3(20181128)");
+println("KeTCindy V.3.2.3(20181201)");
 println(ketjavaversion());
 println("ketcindylibbasic1(20181125) loaded");
 

aead0aaa3ab95356eaa92fd5cce912685767f46a
s-takato <s-takato@jcom.home.ne.jp>
2022-12-28 20:20:06 +0900

    vn24

--- scripts/ketlib/ketcindylibbasic1r.cs
+++ scripts/ketlib/ketcindylibbasic1r.cs
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 
-println("KeTCindy V.4.4.23");
+println("KeTCindy V.4.4.24");
 println(ketjavaversion());
 println("ketcindylibbasic1[20221218] loaded");
 

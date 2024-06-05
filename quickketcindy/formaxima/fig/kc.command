087a9e4b4ad872834e497af1363de476743d121f
s-takato <s-takato@jcom.home.ne.jp>
2024-06-06 06:01:57 +0900

    4.4.82

---
+++ quickketcindy/formaxima/fig/kc.command
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd "/Users/takatoosetsuo/Desktop/ketcindy-4.4.81/quickketcindy/formaxima/fig"
+rm 0comtanans1.txt
+"/Applications/Maxima.app/Contents/Resources/maxima.sh" -b "0comtanans1.max"
+exit 0
--- scripts/ketlib/ketcindylibbasic1r.cs
+++ scripts/ketlib/ketcindylibbasic1r.cs
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 
-println("KeTCindy V.4.4.80");
+println("KeTCindy V.4.4.82");
 println(ketjavaversion());
 println("ketcindylibbasic1[20230801] loaded");
 

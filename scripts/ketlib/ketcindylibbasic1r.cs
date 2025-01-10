dc9bd387c75a4d16f4855ea44a5856dd73b17e8e
s-takato <s-takato@jcom.home.ne.jp>
2025-01-11 08:55:59 +0900

    4.5.10

--- scripts/ketlib/ketcindylibbasic1r.cs
+++ scripts/ketlib/ketcindylibbasic1r.cs
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 
-println("KeTCindy V.4.5.09");
+println("KeTCindy V.4.5.10");
 println(ketjavaversion());
 println("ketcindylibbasic1[20230801] loaded");
 
